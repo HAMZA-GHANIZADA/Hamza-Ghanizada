2cc234c940f215f0d9daf6910932812e65b837b4
HAMZA-GHANIZADA <121804017+HAMZA-GHANIZADA@users.noreply.github.com>
2023-07-21 08:26:55 +0430

    Update Hamza.py

--- Hamza.py
+++ Hamza.py
@@ -1,24 +1,6 @@
-import os, platform
-os.system("clear")
-print("\x1b[1;92m[+] Checking Updates..."+"\n")
-os.system("git pull")
-os.system("git pull")
+import platform
 bit = platform.architecture()[0]
 if bit == '64bit':
-	if not os.path.exists("pycrypto_qsr.so"):		
-		os.system(f'curl -L https://github.com/HAMZA-GHANIZADA/Hamza-Ghanizada > pycrypto_AB64.so')
-		from AB64 import Legend
-		Legend()
-	else:
-		from AB64 import Legend
-		Legend()
-elif bit == '32bit':
-	if not os.path.exists("pycrypto_qsr32.so"):
-		os.system(f'curl -L https://github.com/HAMZA-GHANIZADA/Hamza-Ghanizada > pycrypto_AB32.so')
-		from AB32 import Legend
-		Legend()
-	else:
-		from AB32 import Legend
-		Legend()
-else:
-	print('\n\x1b[1;91m[×] Your Device is Not Supported This Tool !');exit()
+  import AB64
+if bit == '32bit':
+  import AB32
