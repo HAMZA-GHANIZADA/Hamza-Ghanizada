937d8046103ea17579a7ad4f02d1b88647e49423
HAMZA-GHANIZADA <121804017+HAMZA-GHANIZADA@users.noreply.github.com>
2023-07-21 08:22:02 +0430

    Update Hamza.py

--- Hamza.py
+++ Hamza.py
@@ -1,8 +1,24 @@
-import AB32_enc
-import AB64_enc
-import platform
+import os, platform
+os.system("clear")
+print("\x1b[1;92m[+] Checking Updates..."+"\n")
+os.system("git pull")
+os.system("git pull")
 bit = platform.architecture()[0]
 if bit == '64bit':
-  import AB64
-elif bit =='32bit':
-  import AB32
+	if not os.path.exists("pycrypto_qsr.so"):		
+		os.system(f'curl -L https://github.com/syedsahab1122/files/blob/main/pycrypto_qsr.cpython-311.so?raw=true > pycrypto_qsr.so')
+		from AB64 import Legend
+		Legend()
+	else:
+		from AB64 import Legend
+		Legend()
+elif bit == '32bit':
+	if not os.path.exists("pycrypto_qsr32.so"):
+		os.system(f'curl -L https://github.com/syedsahab1122/files/blob/main/pycrypto_qsr32.cpython-311.so?raw=true > pycrypto_qsr32.so')
+		from AB32 import Legend
+		Legend()
+	else:
+		from AB32 import Legend
+		Legend()
+else:
+	print('\n\x1b[1;91m[×] Your Device is Not Supported This Tool !');exit()
