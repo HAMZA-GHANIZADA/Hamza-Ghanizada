76585de64485d9d076eafebded3d8bd49e36ffda
HAMZA-GHANIZADA <121804017+HAMZA-GHANIZADA@users.noreply.github.com>
2023-10-24 09:15:59 +0430

    Update Hamza.py

--- Hamza.py
+++ Hamza.py
@@ -1,1 +1,8 @@
-import AB_enc
+import os
+os.system('git pull')
+from os import system
+from platform import uname
+bt=uname().machine.lower()
+if 'aarch' in bt:
+    import hmxa
+else:exit('\033[1;31m\n Sorry System or 32bit device not supported ')
